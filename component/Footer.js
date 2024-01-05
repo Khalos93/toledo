9be56b47afc8bca76e3d82990958aa8a5054ffdc
Riccardo <r.golin@hotmail.it>
2024-01-05 19:19:12 +0100

    added some detail to footer and some style

--- component/Footer.js
+++ component/Footer.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import './Footer.scss';
 
 function Footer() {
   return (
@@ -12,13 +13,18 @@ function Footer() {
         </div>
         <div className="footer--card">
           <h4 className="footer--card__title">HOURS</h4>
-          <p className="footer--card__details"></p>
-          <p className="footer--card__details"></p>
+          <p className="footer--card__details">Wednesday - Thursday</p>
+          <p className="footer--card__details">17.00 - 21.30</p>
+          <p className="footer--card__details">Friday</p>
+          <p className="footer--card__details">17.00 - 22.00</p>
+          <p className="footer--card__details">Saturday</p>
+          <p className="footer--card__details">13.00 - 21.30</p>
+          <p className="footer--card__details">Sunday</p>
+          <p className="footer--card__details">13.00 - 21.00</p>
         </div>
         <div className="footer--card">
           <h4 className="footer--card__title">CONTACT</h4>
-          <p className="footer--card__details"></p>
-          <p className="footer--card__details"></p>
+          <p className="footer--card__details">00447424378493</p>
         </div>
       </div>
     </footer>
--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
 import './App.css';
+import { BrowserRouter, Route, Router, Routes } from 'react-router-dom';
+import Footer from '../component/Footer';
 
 function App() {
-  return <></>;
+  return (
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route />
+        </Routes>
+        <Footer />
+      </BrowserRouter>
+    </>
+  );
 }
 
 export default App;
